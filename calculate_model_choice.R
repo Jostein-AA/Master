3c2c588b7ae19922afcbac44a5224b64f61bf8ee
Jostein-AA <72163968+Jostein-AA@users.noreply.github.com>
2024-03-13 19:05:48 +0100

    Create calculate_model_choice.R

---
+++ calculate_model_choice.R
@@ -0,0 +1,51 @@
+#Clear environment
+rm(list = ls())
+
+#Load in data/functions and do necessary preliminaries
+source("libraries.R")
+source("Utilities.R")
+
+load("maps_and_nb.RData")
+load("grids_and_mappings.RData")
+
+################################################################################
+# Functions for calculating MSE, IS,...
+
+count_mse_one_year_one_dataset <- function(){
+  
+}
+
+count_mean_mse_one_dataset <- function(){
+  
+}
+
+count_IS_one_year_one_dataset <- function(){
+  
+}
+
+count_mean_IS_one_dataset <- function(){
+  
+}
+
+rate_mse_one_year_one_dataset <- function(){
+  
+}
+
+rate_mean_mse_one_dataset <- function(){
+  
+}
+
+rate_IS_one_year_one_dataset <- function(){
+  
+}
+
+rate_mean_IS_one_dataset <- function(){
+  
+}
+
+
+################################################################################
+# load in data and start calculating
+
+load("Simulated_data/sc1/Improper1_noInt_sc1_1.RData")
+
